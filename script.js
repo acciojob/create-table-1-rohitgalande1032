ab18e6ffb4370098165717248ac4f5a9dea2e061
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-01-20 20:32:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
     //Write your code here
-  
+	let sampleTable = document.getElementById("sampleTable");
+	sampleTable.innerHTML += `
+		<tr>
+			<td>New Cell1</td>
+			<td>New Cell2</td>
+		</tr>
+	`
   
 }
